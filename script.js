dc1cbe4e220b2432cef3825ac836e33731f6ffdf
Apteno(dytes) <88605874+Apteno-dytes@users.noreply.github.com>
2022-08-22 22:55:06 +0900

    🎛️共有URLの再調整

--- script.js
+++ script.js
@@ -42,5 +42,5 @@ function Tweet() {
     var result_text = document.getElementById("result").value;
     result_text += "\n";
     result_text = result_text.replace(/\n/g, "%0A");
-    window.open("https://twitter.com/share?text=" + result_text + "&url=https://apteno-dytes.github.io/HaikuTweeter/", "_blank");
+    window.open("https://twitter.com/intent/tweet?text=" + result_text, "_blank");
 }
